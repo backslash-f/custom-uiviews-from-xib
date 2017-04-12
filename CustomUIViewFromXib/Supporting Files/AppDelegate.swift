3e1d9b8d1278225e929ba2f509d8c622767bdfe3
Fernando Fernandes <mfernandes.fernando@gmail.com>
2017-04-12 20:29:57 -0300

    Updated to Swift 3.

--- CustomUIViewFromXib/Supporting Files/AppDelegate.swift
+++ CustomUIViewFromXib/Supporting Files/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  CustomUIViewFromXib
 //
 //  Created by Fernando Fernandes on 7/21/16.
-//  Copyright © 2016 singledev. All rights reserved.
+//  Copyright © 2016 backslash-f. All rights reserved.
 //
 
 import UIKit
